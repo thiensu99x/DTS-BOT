29e288abae6990af84ce6d38cad3e3934a65e3b3
Thiensu99x <thiensu12123@gmail.com>
2024-10-05 19:54:04 +0700

    first commit

--- utils/log.js
+++ utils/log.js
@@ -24,15 +24,15 @@ module.exports.loader = (data, option) => {
 
   switch (option) {
     case 'warn':
-      coloredData = gradient('#00FFFF' , '#00FF33' , '#FFCCFF' , '#ed3491', '#0000FF' , '#cb34ed' ,'#00FF00' , '#347bed' , '#00EE00').multiline('[ ʙʟᴀᴄᴋ ɢᴏᴋᴜ ]→' + data);
+      coloredData = gradient('#00FFFF' , '#00FF33' , '#FFCCFF' , '#ed3491', '#0000FF' , '#cb34ed' ,'#00FF00' , '#347bed' , '#00EE00').multiline('[ DTS-BOT ]→' + data);
       console.log(chalk.bold(coloredData));
       break;
     case 'error':
-      coloredData = chalk.bold.hex('#FFCCCC')('[ ʙʟᴀᴄᴋ ɢᴏᴋᴜ ]→') + chalk.bold.red(data);
+      coloredData = chalk.bold.hex('#FFCCCC')('[ DTS-BOT ]→') + chalk.bold.red(data);
       console.log(coloredData);
       break;
     default:
-      coloredData = gradient('#ed3491', '#cb34ed', '#347bed','#3366FF' , '#FF3366','#0000FF' , '#00DD00').multiline('[ ʙʟᴀᴄᴋ ɢᴏᴋᴜ ]→ ' + data);
+      coloredData = gradient('#ed3491', '#cb34ed', '#347bed','#3366FF' , '#FF3366','#0000FF' , '#00DD00').multiline('[ DTS-BOT ]→ ' + data);
       console.log(chalk.bold(coloredData));
       break;
   }
